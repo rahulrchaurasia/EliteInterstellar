597cb2077ddd76ef68cdf4827def7c23600614bd
Nilesh Birhade <nileshbirhade@gmail.com>
2021-05-04 17:47:22 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EliteInterstellar
 # EliteInterstellar
+# EliteInterstellar
---
+++ app/src/main/java/com/interstellar/elite/login/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.interstellar.elite.login
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.interstellar.elite.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
