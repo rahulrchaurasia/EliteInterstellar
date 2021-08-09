9e5df8158646dd9a36a167c9a85e649f72600bbf
Rahul <rahulrchaurasia@gmail.com>
2021-08-10 01:10:16 +0530

    doc

--- app/src/main/java/com/interstellar/elite/secure/RSA/RoadSideAssistActivity.kt
+++ app/src/main/java/com/interstellar/elite/secure/RSA/RoadSideAssistActivity.kt
@@ -222,8 +222,8 @@ class RoadSideAssistActivity : BaseActivity(), View.OnClickListener, IResponseSu
         gloabalAssureEntity.CertificateFile.let {
 
 
-           // downloadPdf(it, "Elite_GlobalAssureDocs" + Utility.currentDateTime())
-           downloadPdf("http://elite.interstellar.co.in/uploads/1559381990.pdf", "Elite_GlobalAssureDocs" + Utility.currentDateTime())    // For Testing Purpose
+            downloadPdf(it, "Elite_GlobalAssureDocs" + Utility.currentDateTime())
+          // downloadPdf("http://elite.interstellar.co.in/uploads/1559381990.pdf", "Elite_GlobalAssureDocs" + Utility.currentDateTime())    // For Testing Purpose
         }
     }
 
